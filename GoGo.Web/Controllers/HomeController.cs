f0d1d1f56bb9739d579d894752d64d6f895effa8
Taras Bulba <tbulba.db@gmail.com>
2016-09-05 23:11:38 +0300

    Cleanup

--- GoGo.Web/Controllers/HomeController.cs
+++ GoGo.Web/Controllers/HomeController.cs
@@ -13,19 +13,6 @@ namespace GoGo.Web.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
 
         public IActionResult Error()
         {
